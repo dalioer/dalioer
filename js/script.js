ec86a4ff1d3cb7e804e6cd1fad12974e9dcbce4f
root <root@localhost.localdomain>
2019-01-29 17:11:39 +0800

    Site updated: 2019-01-29 17:11:39

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@
   });
 
   // Caption
-  $('.article-content').each(function(i){
+  $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
       if ($(this).parent().hasClass('fancybox')) return;
 
@@ -107,7 +107,7 @@
   }
 
   // Mobile nav
-  var $fullpage = $('#fullpage'),
+  var $container = $('#container'),
     isMobileNavAnim = false,
     mobileNavAnimDuration = 200;
 
@@ -121,17 +121,17 @@
     }, mobileNavAnimDuration);
   }
 
-  $('#nav-toggle').on('click', function(){
+  $('#main-nav-toggle').on('click', function(){
     if (isMobileNavAnim) return;
 
     startMobileNavAnim();
-    $fullpage.toggleClass('mobile-nav-on');
+    $container.toggleClass('mobile-nav-on');
     stopMobileNavAnim();
   });
 
-  $('#wrapper').on('click', function(){
-    if (isMobileNavAnim || !$fullpage.hasClass('mobile-nav-on')) return;
+  $('#wrap').on('click', function(){
+    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
 
-    $fullpage.removeClass('mobile-nav-on');
+    $container.removeClass('mobile-nav-on');
   });
 })(jQuery);
